c775400a610503c79458f7a11e3ae66c05e6abbb
grryv <grryv@grry>
2020-10-21 19:34:10 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan4/kambing/PBO210116147Latihan4Kambing.java
@@ -0,0 +1,24 @@
+package pbo2.pkg10116147.latihan4.kambing;
+
+/**
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk menampilkan jumlah kambing.
+ */
+
+public class PBO210116147Latihan4Kambing {
+
+    public void tambahKambing() {
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah di tambahkan:  "+jumlahKambing);
+    }
+    public static void main(String[] args) {
+        PBO210116147Latihan4Kambing kambingJantan = new PBO210116147Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
